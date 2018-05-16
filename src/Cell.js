de951766ffd88a36ff34f4e93a5d57a19275e35f
rayGreycloud <rayGreycloud@gmail.com>
2018-05-16 16:56:58 -0700

    Randomize cell color

--- src/Cell.js
+++ src/Cell.js
@@ -5,6 +5,11 @@ class Cell extends Component {
   
   render() {
     const { x, y, cellSize } = this.props;
+    
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);   
+     
     return (
       <div
         className="Cell"
@@ -12,7 +17,8 @@ class Cell extends Component {
           left: `${cellSize * x + 1}px`,
           top: `${cellSize * y + 1}px`,
           width: `${cellSize - 1}px`,
-          height: `${cellSize - 1}px`
+          height: `${cellSize - 1}px`,
+          background: `rgb(${r}, ${g}, ${b})`
         }}  
       />
     );
